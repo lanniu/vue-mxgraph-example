eb3b35aa2b9a2dbd34cb5f1f1d5beb33c802400b
LanNiu <271067929@qq.com>
2019-01-24 17:32:34 +0800

    DONE

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuetify);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
